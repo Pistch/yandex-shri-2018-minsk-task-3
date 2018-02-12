0b88ac1a362cbcefa60c4a593ce7e76010df1cf7
Alexander Glushenko <pistch@yandex.ru>
2018-02-12 14:16:55 +0300

    рок

--- js/main.js
+++ js/main.js
@@ -92,11 +92,7 @@ class Game {
 
   recognizeSpeechSetup() {
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
     let recognition = new SpeechRecognition();
-      //speechRecognitionList = new SpeechGrammarList();
-    //speechRecognitionList.addFromString(this.recognitionGrammar, 1);
-    //recognition.grammars = speechRecognitionList;
     recognition.lang = 'ru-RU';
     recognition.continuous = true;
     recognition.maxAlternatives = 1;
@@ -107,8 +103,9 @@ class Game {
       console.log('Confidence: ' + event.results[0][0].confidence);
     };
 
+    recognition.addEventListener('start', () => console.log('recognition stated'));
+
     recognition.onspeechend = function() {
-      console.log('123');
       recognition.stop();
     };
 
